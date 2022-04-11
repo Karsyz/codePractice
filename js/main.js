5e6a6cce0f933bd5e0ecadb377f226d58d400c98
Karsyz <mattkars@gmail.com>
2022-04-10 23:37:07 -0400

    added html css and main js files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+//  Get the array of each Challenge and populate options list in dom
+
+
+//  Load the default challenge description and code as the home page
+
+//  When a specific challenge is selected from the list
+    //  Display the challenge description
+    //  Display the code from the file
+    //  Change source for the run code button
