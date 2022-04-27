ac46bb25dae03d985e8674dde0293db00e70bb17
Karsyz <mattkars@gmail.com>
2022-04-27 13:03:00 -0400

    added 8kyu switchItUp.js

---
+++ js/7kyu/countStringsInObject.js
@@ -0,0 +1,29 @@
+// Create a function strCount (takes an object as argument) that will count all string values inside an object. For example:
+
+// strCount({
+//   first: "1",
+//   second: "2",
+//   third: false,
+//   fourth: ["anytime",2,3,4],
+//   fifth:  null
+//   })
+//   returns 3
+
+
+function strCount(obj){
+    let count = 0
+    let string = 234234
+    let result = typeof obj
+    
+    
+    return result
+  //   for (const item in obj) {
+  //     if ( isNaN( obj[item] ) )
+  //       count++
+  //   }
+  //   return count
+  }
+
+
+console.log( strCount(234234) === 'number')
+  
