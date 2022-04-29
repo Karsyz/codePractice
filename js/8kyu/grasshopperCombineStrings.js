62169f7a9a69dff7e6b76cffd4569a9003cb5abc
Karsyz <mattkars@gmail.com>
2022-04-29 14:33:00 -0400

    added a test

--- js/8kyu/grasshopperCombineStrings.js
+++ js/8kyu/grasshopperCombineStrings.js
@@ -1,19 +1,14 @@
 // Combine strings function
 // Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
-
 // Example:
-
 // combineNames('James', 'Stevens')
 // returns:
-
 // 'James Stevens'
-
-
 
 function combineNames(a, b) {
     return a + ' ' + b
 }
 
-
 // tests (returns true)
 console.log( combineNames('James', 'Stevens') === 'James Stevens')
+console.log( combineNames('David', 'Rando') === 'David Rando')
