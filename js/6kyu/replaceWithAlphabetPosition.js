38a06406a6e5db1d4d15bf3e4e154f561e0e2d44
Karsyz <mattkars@gmail.com>
2022-04-30 23:50:22 -0400

    organized challenges into kyu folders and solved two problems

--- js/6kyu/replaceWithAlphabetPosition.js
+++ js/6kyu/replaceWithAlphabetPosition.js
@@ -3,22 +3,14 @@ function alphabetPosition(txt) {
     let text = txt.toLowerCase()
     let textNums = ''
     for (i = 0; i < text.length; i++) {
-        
         for (j = 1; j < letterKey.length; j++) {
             if ( text.charAt(i) === letterKey.charAt(j) ) {
-                textNums += `${j}x`
+                textNums += `${j} `
                 break
             }
-        }
+        }                
     }
-
-    console.log(textNums.charAt(textNums.length-1))
-    
-    if (textNums.charAt(textNums.length-1) === 'x') {
-        textNums.replaceAll('x', 'y')
-    }
-
-    return textNums
+    return textNums.slice(0, textNums.length-1)
 }
 
-  console.log(alphabetPosition("The sunset sets at twelve o' clock"))
+  console.log(alphabetPosition("The sunset sets at twelve o' clock....."))
