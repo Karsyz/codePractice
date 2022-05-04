4a074c17e265f20f5001a563926162b3aebf5eb9
Karsyz <mattkars@gmail.com>
2022-05-04 13:21:00 -0400

    added 8kyu checkForFactor.js

---
+++ js/8kyu/checkForFactor.js
@@ -0,0 +1,15 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+
+
+function checkForFactor (base, factor) {
+    return base % factor === 0
+}
+
+
+
+// tests
+
+console.log( checkForFactor(10,2) ) // returns true
