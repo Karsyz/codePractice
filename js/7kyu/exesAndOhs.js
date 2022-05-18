7be447d40e6ee3a5675ef87d36cb84a466f8b481
Karsyz <mattkars@gmail.com>
2022-05-18 08:20:20 -0400

    added exesAndOhs.js

---
+++ js/7kyu/exesAndOhs.js
@@ -0,0 +1,18 @@
+function XO(str) {
+    //sanitized input to array
+    strArr = str.toLowerCase().split('')
+    //declare variables
+    let x = 0
+    let o = 0
+    let other = 0
+    //sort
+    strArr.forEach(element => element === 'x' ? x++ : element === 'o' ? o++ : other++)
+    //conditions
+    return ( x == o || (x == 0 && o == 0) )
+}  
+
+console.log(XO('xo'))   //true
+console.log(XO("xxOo")) //true
+console.log(XO("xxxm")) //false
+console.log(XO("Oo"))   //false
+console.log(XO("ooom")) //false
