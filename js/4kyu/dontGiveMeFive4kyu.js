167703d53022fe42dab5a0cda433b5336182c1e7
Karsyz <mattkars@gmail.com>
2022-06-19 20:11:25 -0400

    changed approcach to dontgiveMefive.js

--- js/4kyu/dontGiveMeFive4kyu.js
+++ js/4kyu/dontGiveMeFive4kyu.js
@@ -19,177 +19,120 @@
 
 // https://www.codewars.com/kata/621f89cc94d4e3001bb99ef4/train/javascript
 
-
-// find the 5's in a number
-
-// count by 1, 10, 100, 1000
-
-// 1        5   in  10      (1 + 0)
-// 19       5's in  100     (9 + 10)
-// 271      5's in  1000    (19 * 9 + 100)
-// 3439     5's in  10000   (271 * 9 + 1000)
-
-// find the 5's in a number
-// count by 1, 10, 100, 1000
-
-
-
-// function dontGiveMeFive(start, end) {
-//     let totalCount = 0
-    
-//     // checks if start number has a 5 digit in it
-//     let startNumArr = Array.from(`${start}`)
-//     // console.log(startNumArr)
-//     for (sn = 0; sn <= startNumArr.length; sn++) {
-//         if (startNumArr[sn] == 5) {
-//             totalCount++
-//             // console.log("added one to total count from start number")
-//             break
-//         }
-//     }
-    
-//     // get number
-
-//     let theNumber = end - start + 1
-//     // console.log(theNumber)
-
-//     // make number array
-
-//     let theNumberArr = Array.from(`${theNumber}`)
-//     // console.log(theNumberArr)
-
-//     // how many digits in number
-
-//     let theNumberLength = Array.from(`${theNumber}`).length
-//     // console.log(theNumberLength)
-
-
-//     // find the 5's in a number
-//     // count by 1, 10, 100, 1000
-
-//     // 1        5   in  10      (1 + 0)
-//     // 19       5's in  100     (9 + 10)
-//     // 271      5's in  1000    (19 * 9 + 100)
-//     // 3439     5's in  10000   (271 * 9 + 1000)
-
-//     function soManyFives(noOfDigits) {
-//         let arr = []
-//         for (r = 10; r <= Math.pow(10,noOfDigits); r = r * 10) {
-//             // console.log(r)
-//             arr.unshift(howManyFives(r))
-//         }
-//         return arr
-//     }
-    
-//     function howManyFives(x) {
-//         let totalFives = 0
-//         for (i = 0; i <= x; i++) {
-//             // convert i into an array of numbers
-//             let numArr = Array.from(`${i}`)
-//             for (j = 0; j <= numArr.length; j++) {
-//                 if (numArr[j] == 5) {
-//                     totalFives++
-//                     break
-//                 }
-//             }
-//         }
-//         return totalFives
-//     }
-
-//     // assign soManyFives to variable
-//     let fivesArr = soManyFives(theNumberLength-1)
-//     // console.log(fivesArr)
-
-
-//     // count with array values from soManyFives()
-
-//     for (k = 0; k <= theNumberLength-2; k++) {
-//         totalCount += theNumberArr[k] * fivesArr[k]
-//         // console.log(theNumberArr[k] * fivesArr[k])
-//         // console.log(totalCount)
-//     }
-
-//     if (theNumberArr[theNumberLength-1] == 5) {
-//         totalCount++
-//         // console.log(totalCount)
-//     }
-
-
-
-//     // console.log(`There are ${totalCount} 5's in the range ${start}-${end}`)
-
-//     return theNumber - totalCount
-
-// }
-
-
-
-
-
+// use math
 
 function dontGiveMeFive(start, end) {
-    let count = 0
-    let total = end - start
-    let totalArr = Array.from(total.toString())
-    console.log(totalArr)
-    // is five count++
     
+    // count range start
+    let arrStart = start.toString().split('').reverse()
+    
+    // count range end
+    let arrEnd = end.toString().split('').reverse()
+    
+    // let arrNum = arr.map(e=> e * 1)
 
-    // count by 1000s?
-    // use algorithm to just count by thousands
+    let startCount = 0
+    let endCount = 0
+    
+    // function that counts first up to 100
+    function countFirst100 (arr) {
+        let counting = 0
+        for (h = 0; h < [arr[1],arr[0]].join('') * 1; h++) {
+            if ( h.toString().search(/5/g) !== -1) {
+                counting++
+            }
+        }
+        return counting
+    }
 
-    // 45129645 the big total
+    // returns qty of five's per digit place
+    // 100 (2) returns 19 for, 1000 (3) returns 271, etc
+    function qtyMulti(num) {
+        let qty = 19
+        for (j = 2; j < num; j++) {
+            qty = 9 * qty + Math.pow(10, j) 
+        }
+        return qty
+    }
+    
+    //count occurances per place 100s+
+    function count(arr) {
+        let counting = 0
+        for (i = 2; i < arr.length; i++) {
+            counting += arr[i] * qtyMulti(i) 
+            
+            // add multiplier if arrNum[i] is greater than 5
+            if (arr[i] > 5) {
+                counting += Math.pow(10, i-1)
+            }
+            
+            // add multiplier and minus qtyMulti(i) if arrNum[i] is equal to 5 
+            if (arr[i] == 5) {
+                counting += Math.pow(10, i-1)
+                counting -= qtyMulti(i)
+            }
+            
+        }
+        return counting
+    }
+    
+    // start count
+    startCount = countFirst100(arrStart) + count(arrStart) 
 
-    // 40000000 40x millions
-    // 5000000 5x millions
-    // 100000 100x thousands
-    // 20000 20x thousands
-    // 9000 9x thousands if > 6000 add 1000 for 5000-5999 range
-    // 645 
+    // end count
+    endCount = countFirst100(arrEnd) + count(arrEnd) 
 
-    // 19
-    // 271     
-    // 3439
+    // return
+    // if count start positive, minus from end
+    // if count start negative, add to end
+    if (startCount > 0) {
+        return endCount - startCount
+    }else {
+        return startCount + endCount
+    }
 
-
-    // if (total < 10000) {
-        // for(i = 1; i <= total; i++) {
-        //     if ( ( i.toString().includes('5') ) ) {
-        //         count++
-        //     }
-        // }
-    // }else {
-    //     console.log('wtf')
-    // }   
-
-    // return count;
 }
 
-// console.log(dontGiveMeFive(1, 10))
-// console.log(dontGiveMeFive(1, 100))
-console.log(dontGiveMeFive(0, 10000))
-// console.log(dontGiveMeFive(1, 10000))
+
+// 0-216
+//    ( 2 * ( (1 * 9) + 10) ) + ( 1 * 1 ) + (1)
+
+// i
+// 0 >= 5 = (1) 
+// 1 1x 1 * 1 (1)  
+// 2 1xx 9 * 1 + 10 (19)
+// 3 1xxx 9 * 19 + 100 (271)
+// 4 1xxxx 9 * 271 + 1000 (3439)
+// 5 4xxxxx 9 * 3439 + 10000 (21439)
+
+
+
+// console.log(dontGiveMeFive(1, 10))  // 1
+// console.log(dontGiveMeFive(1, 100)) // 19
+
+// console.log(dontGiveMeFive(0, 1000)) // 271
+// console.log(dontGiveMeFive(1, 10000)) // 2194
 // console.log(dontGiveMeFive(1, 100000))
 // console.log(dontGiveMeFive(1, 1000000))
 
+// console.log(dontGiveMeFive(-17, 9))  // 24
+// console.log(dontGiveMeFive(1, 9))  // 8
+// console.log(dontGiveMeFive(4, 17))  // 12
+// console.log(dontGiveMeFive(-17, -4))  // 12
 
-// console.log(dontGiveMeFive(-17, 9))
-// console.log(dontGiveMeFive(1, 9))
-// console.log(dontGiveMeFive(4, 17))
-// console.log(dontGiveMeFive(-17, -4))
+console.log(dontGiveMeFive(984, 4304)) // 2449
+console.log(dontGiveMeFive(2313, 2317)) // 4
+console.log(dontGiveMeFive(-4045, 2575)) // 4819
+console.log(dontGiveMeFive(-4436, -1429)) // 2194
 
-// console.log(dontGiveMeFive(984, 4304))
-// console.log(dontGiveMeFive(2313, 2317))
-// console.log(dontGiveMeFive(-4045, 2575))
-// console.log(dontGiveMeFive(-4436, -1429))
+// console.log(dontGiveMeFive(0, 1)) // 2
+// console.log(dontGiveMeFive(5, 15)) // 9
+// console.log(dontGiveMeFive(-5, 4)) // 9
+// console.log(dontGiveMeFive(51, 60)) // 1
 
-// console.log(dontGiveMeFive(0, 1))
-// console.log(dontGiveMeFive(5, 15))
-// console.log(dontGiveMeFive(-5, 4))
-// console.log(dontGiveMeFive(51, 60))
+// console.log(dontGiveMeFive(40076, 215151422963990)) // 49707598394353
+// console.log(dontGiveMeFive(-206981731, 223575697903165)) // 51841599744277
+// console.log(dontGiveMeFive(-249022878360451, -249022878219653)) // 79380    
+// console.log(dontGiveMeFive(-90000000000000, 900000000000000)) // 203349266266321
 
 
-// console.log(dontGiveMeFive(40076, 215151422963990))
-// console.log(dontGiveMeFive(-206981731, 223575697903165))
-// console.log(dontGiveMeFive(-249022878360451, -249022878219653))
-// console.log(dontGiveMeFive(-90000000000000, 900000000000000))
