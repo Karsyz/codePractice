932f0770eddf9ef96004c71d225db361c15dc213
Karsyz <mattkars@gmail.com>
2022-08-13 21:26:37 -0400

    added 8kyu twiceAsOld.js

---
+++ js/8kyu/twiceAsOld.js
@@ -0,0 +1,7 @@
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs( dadYearsOld - (sonYearsOld * 2) )
+}
+
+console.log( twiceAsOld(88,32) ) // 24 years ago
